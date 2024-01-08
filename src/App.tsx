cc3202c0071339f644a1f73a108829b8c0e3f293
dc-21 <cholahkuboko@gmail..com>
2024-01-08 23:45:56 +0200

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./component/Navbar";
-import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col items-center">
       <Navbar />
-      <div className="w-full max-w-7xl mt-4 flex-col flex items-center justify-center bg-green-400">
-        <Routes>
-          <Route path="/" Component={Home} />
-        </Routes>
-      </div>
-    </>
+    </div>
   );
 }
 
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const NavBar = () => {
+  const [open, setOpen] = useState<boolean>(false);
   return (
-    <nav className="w-full py-4 px-4">
-      <Link to="/">Relevant Network</Link>
+    <nav className="flex items-center max-w-7xl fixed text-white mt-2 bg-[#5355e0] top-0 z-50 bg-opacity-[98%] backdrop-blur w-full justify-between px-2 md:px-6 py-7">
+      <Link to={"/"} className="text-2xl">
+        RELEVANT NETWORK
+      </Link>
+      <li className="list-none gap-4 flex">
+        <Link to={"/"}>HOME</Link>
+        <Link to={"/"}>ABOUT</Link>
+        <Link to={"/"}>SERVICES</Link>
+        <Link to={"/"}>BLOG</Link>
+        <Link to={"/"}>CONTACT</Link>
+      </li>
     </nav>
   );
 };
 
-export default Navbar;
+export default NavBar;
