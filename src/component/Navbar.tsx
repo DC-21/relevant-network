43ffc3ce405e6342b49958a306961d1e2f084555
dc-21 <cholahkuboko@gmail..com>
2024-01-09 00:05:17 +0200

    initial

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -30,6 +30,31 @@ const NavBar = () => {
           } bg-white duration-300 delay-150`}
         ></div>
       </button>
+      <div
+        className={`absolute origin-bottom ${
+          open ? "scale-y-100" : "scale-y-0"
+        } md:hidden top-[101%] bg-[#5658e0] max-w-7xl delay-150 duration-300 bg-opacity-[98%] backdrop-blur-md w-full inset-x-0`}
+      >
+        <div className=" text-2xl flex min-h-screen flex-col h-full w-full gap-4 items-center list-none">
+          <div className=" text-center flex flex-col mt-20 gap-2">
+            <li>
+              <Link to={"/blog"}>HOME</Link>
+            </li>
+            <li>
+              <Link to={"/about"}>ABOUT</Link>
+            </li>
+            <li>
+              <Link to={"/services"}>SERVICES</Link>
+            </li>
+            <li>
+              <Link to={"/services"}>BLOG</Link>
+            </li>
+            <li>
+              <Link to={"/services"}>CONTACTS</Link>
+            </li>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
