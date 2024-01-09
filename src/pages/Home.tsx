41a5fe849d3d2d2b5e34a14c204430d36cee3924
dc-21 <cholahkuboko@gmail..com>
2024-01-09 23:03:42 +0200

    home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,10 +5,10 @@ const Home = () => {
     <section className="flex w-full max-w-7xl h-auto py-20 px-4 justify-center items-center">
       <div className="w-full flex gap-4 justify-center items-center text-black">
         <div className="w-1/2 justify-center">
-          <h1 className="text-[60px] font-weight: 800 text-[#1b1212]">
+          <h1 className="text-[60px] font-weight: 800 text-[#353935]">
             How We Help Women
           </h1>
-          <p className="text-[#2c150b] text-lg">
+          <p className="text-[#353935] text-lg">
             To support women thereby transforming generations; enabling them to
             live full lives. Through creating a platform; for them to access
             vocational skills training, business opportunities, income
@@ -16,7 +16,7 @@ const Home = () => {
             care, shelter, sanitation and education services.
           </p>
           <div className="flex gap-6 mt-6">
-            <button className="py-3 px-4 text-white rounded bg-[#2c150b] hover:bg-[#633a28]">
+            <button className="py-3 px-4 text-white rounded bg-[#353935] hover:bg-[#495749]">
               Donate Now
             </button>
             <button className="bg-[#4d9136] hover:bg-[#64be46] text-white rounded py-3 px-4">
