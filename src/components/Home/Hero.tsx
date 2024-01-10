e919dde89a17b3ab676df76f8124f604f964e223
tamani phiri <tamanigabriel0@gmail.com>
2024-01-10 12:40:16 +0200

    c

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,17 +1,23 @@
+import bg from "../../assets/homehero.jpg";
 const Hero = () => {
   return (
-    <header className="">
-      <h1 className="">How We Help Women</h1>
-      <p className="">
-        To support women thereby transforming generations; enabling them to live
-        full lives. Through creating a platform; for them to access vocational
-        skills training, business opportunities, income generating activities
-        allowing them to access; good quality health care, shelter, sanitation
-        and education services.
-      </p>
-      <div className="">
-        <button className="">Donate Now</button>
-        <button className="">Disover +</button>
+    <header
+      style={{ backgroundImage: `url(${bg})` }}
+      className="w-full flex flex-col items-center justify-center bg-cover bg-center bg-no-repeat"
+    >
+      <div className="min-h-screen bg-[#ff6600] bg-opacity-50">
+        <h1 className="">How We Help Women</h1>
+        <p className="">
+          To support women thereby transforming generations; enabling them to
+          live full lives. Through creating a platform; for them to access
+          vocational skills training, business opportunities, income generating
+          activities allowing them to access; good quality health care, shelter,
+          sanitation and education services.
+        </p>
+        <div className="">
+          <button className="">Donate Now</button>
+          <button className="">Disover +</button>
+        </div>
       </div>
     </header>
   );
