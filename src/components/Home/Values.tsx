69cfceedf2955be409e408e36d8cf6e5762e39dc
tamani phiri <tamanigabriel0@gmail.com>
2024-01-10 13:55:12 +0200

    c

--- src/components/Home/Values.tsx
+++ src/components/Home/Values.tsx
@@ -1,6 +1,6 @@
 const Values = () => {
   return (
-    <section className="min-h-screen w-full flex flex-col px-4 md:px-12">
+    <section className="min-h-screen w-full flex flex-col bg-[#ffcc99] bg-opacity-50 px-4 md:px-12 py-16">
       <h1 className="text-center text-4xl font-bold">WE CAN CHANGE THE WORLD</h1>
       <div className="grid md:grid-cols-4 grid-cols-1 w-full items-center justify-center gap-4">
         <div className="">
