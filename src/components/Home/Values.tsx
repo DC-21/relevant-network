a4fe06114b73f849f9681d64bc6b81204179b3fc
tamani phiri <tamanigabriel0@gmail.com>
2024-01-10 16:31:23 +0200

    c

--- src/components/Home/Values.tsx
+++ src/components/Home/Values.tsx
@@ -1,9 +1,31 @@
+import bg from "../../assets/her03.jpg";
 const Values = () => {
   return (
-    <div>
-      values
+    <div className="min-h-screen px-4 md:px-12 py-16 items-center justify-center grid md:grid-cols-2 w-full">
+      <div>
+        <img
+          src={bg}
+          alt="woman"
+          loading="lazy"
+          className=" w-full object-cover object-center"
+        />
+      </div>
+      <div>
+        <h1 className="text-center md:text-left text-4xl text-gray-700 font-bold">
+          LEGAL STRUCTURE
+        </h1>
+        <p className="md:text-xl text-lg text-gray-600">
+          Relevant shall be a legally registered as a Non-Governmental
+          Organization (NGO) according to the laws of Zambia.
+        </p>
+        <div className=" flex items-center justify-center md:justify-start gap-2 w-full my-2">
+          <div className=" h-3 bg-[#ff6699] w-8"></div>
+          <div className=" h-1 bg-gray-400 w-8"></div>
+          <div className=" h-1 bg-gray-400 w-8"></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Values
+export default Values;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
     <>
       <Hero />
       <Objectives />
-      <Values/>
+      <Values />
     </>
   );
 };
