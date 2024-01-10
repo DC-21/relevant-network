7ae75e45942627e4ba28993f7b8f898f33f1f6c8
tamani phiri <tamanigabriel0@gmail.com>
2024-01-10 12:46:11 +0200

    c

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -3,11 +3,11 @@ const Hero = () => {
   return (
     <header
       style={{ backgroundImage: `url(${bg})` }}
-      className="w-full bg-cover bg-center bg-no-repeat"
+      className="w-full bg-cover bg-center bg-no-repeat  text-white"
     >
-      <div className="min-h-screen bg-black bg-opacity-40 flex gap-4 flex-col items-center justify-center text-center backdrop-blur-[2px]">
-        <h1 className=" text-white font-bold text-5xl">How We Help Women</h1>
-        <p className="lg:max-w-xl">
+      <div className="min-h-screen bg-black bg-opacity-40 flex gap-4 flex-col px-4 md:px-10 items-center justify-center text-center backdrop-blur-[2px]">
+        <h1 className="font-bold text-5xl">How We Help Women</h1>
+        <p className="lg:max-w-xl md:text-xl text-lg">
           To support women thereby transforming generations; enabling them to
           live full lives. Through creating a platform; for them to access
           vocational skills training, business opportunities, income generating
