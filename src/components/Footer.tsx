611c14d249e21ad5553da272e02cf601249a1960
tamani phiri <tamanigabriel0@gmail.com>
2024-01-10 19:05:12 +0200

    c

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+
 const Footer = () => {
   return (
     <footer>
-      
+      <div>
+        <Link to={"/"}>
+          <img src={logo} loading="lazy" alt="logo" />
+        </Link>
+      </div>
+      <div></div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
