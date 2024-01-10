717b2f6b6ac8f4f33aa617d3d65dfa912ed901d7
tamani phiri <tamanigabriel0@gmail.com>
2024-01-10 19:07:15 +0200

    c

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,10 @@ import logo from "../assets/logo.png";
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
+    <footer className=" flex flex-col w-full px-4 md:px-12">
+      <div className=" flex md:flex-row justify-between flex-col items-center">
         <Link to={"/"}>
-          <img src={logo} loading="lazy" alt="logo" />
+          <img src={logo} loading="lazy" alt="logo" className=" h-12"/>
         </Link>
       </div>
       <div></div>
