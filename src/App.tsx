dee56445550b4e2c22c6d2cc33e2fb03bc136b65
tamani phiri <tamanigabriel0@gmail.com>
2024-01-10 14:33:14 +0200

    c

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="flex flex-col items-center max-w-7xl w-full justify-center">
+    <div className="flex flex-col items-center max-w-7xl w-full justify-center mx-auto">
       <Navbar />
       <Routes>
         <Route path="/" Component={Home} />
