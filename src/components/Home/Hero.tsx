95bce712f7025bb3fbbf8fad1eb13f9470e5c300
tamani phiri <tamanigabriel0@gmail.com>
2024-03-26 11:33:32 +0200

    chanegs

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
       className="w-full bg-cover bg-center bg-no-repeat  text-white"
     >
       <div className="min-h-screen bg-black bg-opacity-40 flex gap-4 flex-col px-4 md:px-10 items-center justify-center text-center backdrop-blur-[2px]">
-        <h1 className="font-bold text-5xl">How We Help Women</h1>
+        <h1 className="font-bold text-5xl"> Wheels Of Hope</h1>
         <p className="lg:max-w-xl md:text-xl text-lg text-gray-200">
           To support women thereby transforming generations; enabling them to
           live full lives. Through creating a platform; for them to access
